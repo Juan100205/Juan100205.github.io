babc2a1b65f96c201236712e44646f63253dc98b
Juan1002005 <juanjorianozabaleta@gmail.com>
2024-06-14 08:51:49 -0500

    Changes added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
-
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://Juan100205.github.io',
+  base: 'mi-repo',
   integrations: [tailwind()]
-});
+
+})
+
